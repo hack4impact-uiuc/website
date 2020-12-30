9ca161d2eb97ccb104409fc31aabaa39639797fd
Arpan Laha <arpanlaha99@gmail.com>
2020-12-30 06:52:53 +0000

    add initial navlink animations

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -15,31 +15,27 @@
 		align-items: center;
 	}
 
+	#logo-link {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
 	img {
 		height: 2em;
 	}
 
-	ul {
+	.navlinks {
 		margin: 0;
 		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: "";
-		display: block;
-		clear: both;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
 	}
 
 	h2 {
 		font-size: 16px;
 		margin: 0;
-	}
-
-	li {
-		display: block;
-		float: left;
-		text-align: center;
 	}
 
 	[aria-current] {
@@ -51,24 +47,40 @@
 	[aria-current]::after {
 		position: absolute;
 		content: "";
-		width: calc(100% - 1em);
-		height: 2px;
+		width: 100%;
+		height: 4px;
 		background-color: #155da1;
 		display: block;
 		bottom: 0px;
-		left: 0.5em;
+		left: 0;
 	}
 
-	li a {
+	.navlink {
 		text-decoration: none;
 		padding: 1em 1em;
-		display: block;
+		display: inline-block;
+		position: relative;
+		transition: 300ms;
 	}
 
-	#logo-link {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
+	.navlink:not([aria-current])::before {
+		height: 0;
+		width: 100%;
+		bottom: 0;
+		left: 0;
+		transition: 300ms;
+		content: "";
+		position: absolute;
+		background-color: #3e87cd;
+	}
+
+	.navlink:not([aria-current]):hover {
+		color: #3e87cd;
+		transition: 300ms;
+	}
+
+	.navlink:not([aria-current]):hover::before {
+		height: 4px;
 	}
 </style>
 
@@ -76,30 +88,26 @@
 	<a id="logo-link" rel="prefetch" href="."><img
 			src="/logo.svg"
 			alt="Hack4Impact logo" /></a>
-	<ul>
-		<li>
-			<a
-				rel="prefetch"
-				aria-current={segment === undefined ? 'page' : undefined}
-				href="."><h2>Home</h2></a>
-		</li>
-		<li>
-			<a
-				rel="prefetch"
-				aria-current={segment === 'about' ? 'page' : undefined}
-				href="about"><h2>About Us</h2></a>
-		</li>
-		<li>
-			<a
-				rel="prefetch"
-				aria-current={segment === 'projects' ? 'page' : undefined}
-				href="projects"><h2>Projects</h2></a>
-		</li>
-		<li>
-			<a
-				rel="prefetch"
-				aria-current={segment === 'join' ? 'page' : undefined}
-				href="join"><h2>Work With Us</h2></a>
-		</li>
-	</ul>
+	<div class="navlinks">
+		<a
+			class="navlink"
+			rel="prefetch"
+			aria-current={segment === undefined ? 'page' : undefined}
+			href="."><h2>Home</h2></a>
+		<a
+			class="navlink"
+			rel="prefetch"
+			aria-current={segment === 'about' ? 'page' : undefined}
+			href="about"><h2>About Us</h2></a>
+		<a
+			class="navlink"
+			rel="prefetch"
+			aria-current={segment === 'projects' ? 'page' : undefined}
+			href="projects"><h2>Projects</h2></a>
+		<a
+			class="navlink"
+			rel="prefetch"
+			aria-current={segment === 'join' ? 'page' : undefined}
+			href="join"><h2>Work With Us</h2></a>
+	</div>
 </nav>
