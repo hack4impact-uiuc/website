6192241c534cceade711f9b354085e8c16c93abe
Arpan Laha <arpanlaha99@gmail.com>
2021-01-02 04:13:33 +0000

    randomize featured project order

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,6 +15,15 @@
 	import Section from "../components/Section.svelte";
 
 	export let projects: Project[];
+
+	function shuffle(array: any[]): void {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+	}
+
+	$: shuffle(projects);
 </script>
 
 <style>
