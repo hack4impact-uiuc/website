5e74240e68f2805a79b597a3c7f2b213511464e7
Arpan Laha <arpanlaha99@gmail.com>
2021-01-02 02:33:24 +0000

    add full project and member types

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,3 +1,31 @@
+export interface Image {
+  src: string;
+  alt: string;
+}
+
+export interface Member {
+  name: string;
+  picture: Image;
+  role: string;
+  linkedin: string;
+  active: boolean;
+  graduationYear: number;
+}
+
 export interface Project {
   name: string;
+  subname: string;
+  semester: string[];
+  slug: string;
+  summary: string;
+  nonprofitDescription: string; // TODO: is this html?
+  nonprofitUrl: string;
+  nonprofitLogo: Image;
+  accentColor: string;
+  // fullDescription: richText??
+  productManager: Member[];
+  techLead: Member[];
+  productDesigner: Member[];
+  softwareDevelopers: Member[];
+  featured: boolean;
 }
