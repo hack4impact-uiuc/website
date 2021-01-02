0143745174bbae5a81c8d60202fc24b589ff2cdd
Arpan Laha <arpanlaha99@gmail.com>
2021-01-02 20:33:48 +0000

    add subpages to about and join

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -5,3 +5,6 @@
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+
+<a href="about/work" rel="prefetch">How We Work</a>
+<a href="about/team" rel="prefetch">Meet the Team</a>
--- src/routes/join/index.svelte
+++ src/routes/join/index.svelte
@@ -5,3 +5,7 @@
 <h1>Work With Us</h1>
 
 <p>This is the 'work with us' page. There's not much here.</p>
+
+<a href="join/students" rel="prefetch">Students</a>
+<a href="join/nonprofits" rel="prefetch">Nonprofits</a>
+<a href="join/sponsors" rel="prefetch">Sponsors</a>
