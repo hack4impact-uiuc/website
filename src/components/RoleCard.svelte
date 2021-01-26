9bf202f058c91f0f0dd61fc11a3a89065bbba38f
Arpan Laha <arpanlaha99@gmail.com>
2021-01-26 05:10:49 +0000

    add roles to how we work page

---
+++ src/components/RoleCard.svelte
@@ -0,0 +1,22 @@
+<div>
+  <h4><slot name="role" /></h4>
+  <p><slot name="description" /></p>
+  <b><slot name="number" /> per team</b>
+</div>
+
+<style>
+  div {
+    background-color: var(--gray-lighter);
+    padding: 20px;
+  }
+
+  h4,
+  b {
+    color: var(--blue-darker);
+  }
+
+  p,
+  b {
+    font-size: 16px;
+  }
+</style>
--- src/routes/about/work.svelte
+++ src/routes/about/work.svelte
@@ -1,4 +1,5 @@
 <script lang="ts" context="module">
+  import RoleCard from "../../components/RoleCard.svelte";
   import Section from "../../components/Section.svelte";
   import Value from "../../components/Value.svelte";
 </script>
@@ -65,9 +66,57 @@
     </Value>
   </div>
 </Section>
+<Section id="teams" padding="40px">
+  <h2>Our Project Teams</h2>
+  <div id="roles">
+    <RoleCard>
+      <span slot="role">Software Developers</span>
+      <span slot="description"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore aliqua. sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua adipiscing elit.</span
+      >
+      <span slot="number">4-5</span>
+    </RoleCard>
+    <RoleCard>
+      <span slot="role">Product Designers</span>
+      <span slot="description"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore aliqua. sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua adipiscing elit.</span
+      >
+      <span slot="number">1</span>
+    </RoleCard>
+    <RoleCard>
+      <span slot="role">Tech Leads</span>
+      <span slot="description"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore aliqua. sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua adipiscing elit.</span
+      >
+      <span slot="number">1</span>
+    </RoleCard>
+    <RoleCard>
+      <span slot="role">Product Manager</span>
+      <span slot="description"
+        >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore aliqua. sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua adipiscing elit.</span
+      >
+      <span slot="number">1</span>
+    </RoleCard>
+  </div>
+</Section>
 
 <style>
   #timeline-steps {
     padding: 20px 0;
+  }
+
+  #roles {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    column-gap: 40px;
+    row-gap: 40px;
   }
 </style>
