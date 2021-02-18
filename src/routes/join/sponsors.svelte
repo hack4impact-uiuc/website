255d74a7b1a0fa226d19aa7d3dfa1b424757660f
Siraj <sirajchokshi@gmail.com>
2021-02-17 22:47:50 -0600

    add sponsor tiers

--- src/routes/join/sponsors.svelte
+++ src/routes/join/sponsors.svelte
@@ -1,15 +1,44 @@
-<script lang="ts">
+<script lang="ts" context="module">
   import Section from '../../components/Section.svelte';
   import Info from '../../components/Info.svelte';
   import Testimonial from '../../components/Testimonial.svelte';
+  import Accordion from '../../components/Accordion.svelte';
+
+  import type { FAQ } from "../../utils/schema";
 </script>
 
-<script lang="ts" context="module">
+<script lang="ts">
   interface Statistic {
     value: string;
     name: string;
   }
+
   let stats: Statistic[] = [1,2,3,4,5,6,7,8].map((n) => ({value: n+"%", name: "stat-" + n} as Statistic));
+
+  interface SponsorTier {
+    name: string;
+    price: number;
+    perks: string[];
+  }
+
+  let tiers: SponsorTier[] = [
+    {
+      name: "Standard",
+      price: 1000,
+      perks: ['Digital Branding', 'Resume Book', 'Standard Session']
+    },
+    {
+      name: "Premier",
+      price: 2000,
+      perks: ['Digital Branding', 'Resume Book', 'Premium Session', 'Coffee Chats']
+    }
+  ]
+
+  let allPerks: string[] = [... new Set(tiers
+    .map((tier) => tier.perks)
+    .reduce((totalPerks: string[], perks: string[]) => totalPerks.concat(perks)))]
+
+  let faqs: FAQ[] = [];
 </script>
 
 <svelte:head>
@@ -56,6 +85,48 @@
   />
 </Section>
 
+<Section color="var(--blue)" padding="60px" >
+  <span class="light-text wrap">
+    <h2>Sponsorship Tiers</h2>
+    <div class="sponsor-perks">
+      {#each tiers as tier}
+        <div class="tier">
+          <h3>{tier.name} Sponsor</h3>
+          <h4>{tier.price}$/semester</h4>
+          <ul>
+            {#each allPerks as perk}
+              <li class={tier.perks.includes(perk) ? '' : 'disabled'}>
+                <span class="status">
+                  {#if tier.perks.includes(perk)}
+                    &checkmark;
+                  {:else}
+                    &times;
+                  {/if}                
+                </span>
+                {perk}
+              </li>
+            {/each}
+          </ul>
+        </div>
+      {/each}
+    </div>
+  </span>
+</Section>
+
+<Section padding="60px">
+  <h2>Where We Work</h2>
+</Section>
+
+<Section padding="40px" id="faq" color="var(--gray-lighter)">
+  <h2>Frequently Asked Questions</h2>
+  {#each faqs as faq}
+    <Accordion>
+      <span slot="title">{faq.question}</span>
+      <span slot="contents">{faq.answer}</span>
+    </Accordion>
+  {/each}
+</Section>
+
 <style>
   p {
     opacity: 80%;
@@ -64,6 +135,16 @@
   .sponsor-intro {
     display: flex;
     flex-direction: row;
+  }
+
+  .light-text {
+    color: #fff;
+  }
+
+  .wrap {
+    width: 100%;
+    display: block;
+    margin: 0 auto;
   }
 
   #sponsor-intro-left {
@@ -108,5 +189,45 @@
   .stat > div > .val {
     font-size: 48px;
     line-height: 1;
+  }
+
+  .sponsor-perks {
+    margin: 70px auto;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .sponsor-perks .tier {
+    display: block;
+    margin: 0 100px;
+  }
+
+  .sponsor-perks h3 {
+    font-size: 32px;
+    margin-bottom: 0;
+  }
+
+  .sponsor-perks h4 {
+    font-size: 24px;
+  }
+
+  .sponsor-perks ul {
+    padding-left: 0;
+    list-style: none;
+    font-size: 20px;
+    line-height: 2;
+  }
+
+  .sponsor-perks ul > li.disabled {
+    opacity: 0.6;
+    user-select: none;
+    cursor: disabled;
+  }
+
+  .sponsor-perks ul > li > .status {
+    user-select: none;
+    display: inline-block;
+    margin-right: .75em;
   }
 </style>
