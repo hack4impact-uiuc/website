cf8f42d396560e03a2b661ce5c22899e74668ac0
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-04-20 00:19:03 -0500

    add Row utility for column-based layouts

---
+++ src/components/Row.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let reverse: boolean;
+    export let gap: number | undefined;
+</script>
+
+
+<article class:reverse={reverse} style="--row-gap: {gap ?? 0}px">
+    <slot />
+</article>
+
+<style>
+    article {
+        display: flex;
+    } 
+
+    article.reverse {
+        flex-direction: row-reverse;
+    }
+
+    article > :global(*) {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        flex: 1;
+    }
+
+    article > :global(*) + :global(*) {
+        margin-left: var(--row-gap);
+    }
+</style>
