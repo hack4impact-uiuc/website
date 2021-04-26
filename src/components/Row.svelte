029ab3b72107374ded479c3bc887f86a10ab7e77
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-04-26 13:59:46 -0500

    add turnary logic to row

--- src/components/Row.svelte
+++ src/components/Row.svelte
@@ -4,7 +4,7 @@
 </script>
 
 
-<article class:reverse={reverse} style="--row-gap: {gap ?? 0}px">
+<article class={reverse ? 'reverse' : ''} style="--row-gap: {gap ?? 0}px">
     <slot />
 </article>
 
