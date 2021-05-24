6395f5b2f3573ec889e3bcfa1ca6a599a361967e
Siraj <19193347+SirajChokshi@users.noreply.github.com>
2021-05-24 11:32:55 -0400

    add move import outside module

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,25 +1,27 @@
 <script context="module" lang="ts">
-  import { stores } from "@sapper/app";
   import Nav from "../components/Nav.svelte";
   import Footer from "../components/Footer.svelte";
   import { generateProjectsInfo } from "../utils/projects";
   import type { Project } from "../utils/schema";
 
-  export async function preload() {
+  export async function preload(page) {
     const res = await this.fetch(`server/projects.json`);
 
     const projects: Project[] = await res.json();
     const { semesters } = generateProjectsInfo(projects);
+
     return { semesters };
   }
 </script>
 
 <script lang="ts">
+  import { stores } from "@sapper/app";
   const { page } = stores();
 
   export let segment: string;
   export let semesters: string[] = [];
 
+  let path = "";
   $: path = $page.path;
 </script>
 
