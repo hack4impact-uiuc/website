2aa4c5ef1d768d9d48cad26df99fb519d56ce4c1
Arpan Laha <arpanlaha99@gmail.com>
2021-09-29 18:29:32 +0000

    move members processing to server

--- src/routes/server/members.json.ts
+++ src/routes/server/members.json.ts
@@ -6,7 +6,28 @@ export async function get(): Promise<any> {
     order: "fields.name",
   });
 
+  const roles = [
+    "Co-Founder",
+    "Co-Director",
+    "Community Director",
+    "External Director",
+    "Tech Director",
+    "Product Manager",
+    "Tech Lead",
+    "Academy Lead",
+    "Product Research Lead",
+    "Product Designer",
+    "Software Developer",
+    "Academy Member",
+  ];
+
+  members.sort((a, b) => a.graduationYear - b.graduationYear);
+  members.sort((a, b) => roles.indexOf(a.role) - roles.indexOf(b.role));
+
   return {
-    body: members,
+    body: {
+      active: members.filter((member) => member.active),
+      alumni: members.filter((member) => !member.active),
+    },
   };
 }
