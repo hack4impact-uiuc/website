931605c91eb6d6cd65d39f5142f5b0f43591a309
Arpan Laha <arpanlaha99@gmail.com>
2021-09-30 00:14:31 +0000

    add member image script

---
+++ scripts/loadMemberImages.js
@@ -0,0 +1,32 @@
+import { readFileSync } from "fs";
+import { promisify } from "util";
+import { exec } from "child_process";
+
+const execPromise = promisify(exec);
+
+async function run() {
+  await execPromise("mkdir -p build/members");
+
+  const fileContents = readFileSync("build/server/members.json");
+  const members = JSON.parse(fileContents.toString());
+  const allMembers = members.active.concat(members.alumni);
+
+  await Promise.all(allMembers.map(loadMemberImage));
+}
+
+async function loadMemberImage(member) {
+  const url = `${member.picture.src}?h=1000&fmt=jpg`.replace("////", "//");
+  const nameSnakeCase = member.name
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "")
+    .toLowerCase()
+    .replace(/ /g, "_")
+    .replace(/-/g, "");
+
+  const filePath = `build/members/${nameSnakeCase}.jpg`;
+
+  await promisify(exec)(`curl -o ${filePath} ${url}`);
+  console.log(`Loaded ${filePath}`);
+}
+
+run();
