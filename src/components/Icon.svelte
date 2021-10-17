ce31ae5315a67e9267f098305873c00b45c82e6e
Arpan Laha <arpanlaha99@gmail.com>
2021-10-17 20:20:49 +0000

    make icon into dynamic component

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,49 +1,53 @@
 <script context="module" lang="ts">
   import Brain from "@svicons/boxicons-regular/brain.svelte";
+  import Bulb from "@svicons/boxicons-solid/bulb.svelte";
+  import Calendar from "@svicons/boxicons-solid/calendar.svelte";
   import Code from "@svicons/boxicons-regular/code-alt.svelte";
+  import Conversation from "@svicons/boxicons-solid/conversation.svelte";
+  import Devices from "@svicons/boxicons-solid/devices.svelte";
   import Edit from "@svicons/boxicons-regular/edit.svelte";
+  import Heart from "@svicons/boxicons-solid/heart.svelte";
   import Question from "@svicons/boxicons-regular/question-mark.svelte";
   import Search from "@svicons/boxicons-regular/search.svelte";
-  import Transfer from "@svicons/boxicons-regular/transfer-alt.svelte";
-  import Bulb from "@svicons/boxicons-solid/bulb.svelte";
-  import Heart from "@svicons/boxicons-solid/heart.svelte";
-  import Conversation from "@svicons/boxicons-solid/conversation.svelte";
-  import Calendar from "@svicons/boxicons-solid/calendar.svelte";
   import Slideshow from "@svicons/boxicons-solid/slideshow.svelte";
-  import Devices from "@svicons/boxicons-solid/devices.svelte";
+  import Transfer from "@svicons/boxicons-regular/transfer-alt.svelte";
   import Vector from "@svicons/boxicons-solid/vector.svelte";
+  import type { SvelteComponent } from "svelte";
+
+  type IconType =
+    | "brain"
+    | "bulb"
+    | "calendar"
+    | "code"
+    | "conversation"
+    | "devices"
+    | "edit"
+    | "heart"
+    | "question"
+    | "search"
+    | "slideshow"
+    | "transfer"
+    | "vector";
+
+  const iconMap: Record<IconType, typeof SvelteComponent> = {
+    brain: Brain,
+    bulb: Bulb,
+    calendar: Calendar,
+    code: Code,
+    conversation: Conversation,
+    devices: Devices,
+    edit: Edit,
+    heart: Heart,
+    question: Question,
+    search: Search,
+    slideshow: Slideshow,
+    transfer: Transfer,
+    vector: Vector,
+  };
 </script>
 
 <script lang="ts">
-  export let icon: string;
+  export let icon: IconType;
 </script>
 
-{#if icon === "brain"}
-  <Brain />
-{:else if icon === "code"}
-  <Code />
-{:else if icon === "edit"}
-  <Edit />
-{:else if icon === "question"}
-  <Question />
-{:else if icon === "bulb"}
-  <Bulb />
-{:else if icon === "heart"}
-  <Heart />
-{:else if icon === "conversation"}
-  <Conversation />
-{:else if icon === "calendar"}
-  <Calendar />
-{:else if icon === "search"}
-  <Search />
-{:else if icon === "slideshow"}
-  <Slideshow />
-{:else if icon === "transfer"}
-  <Transfer />
-{:else if icon === "devices"}
-  <Devices />
-{:else if icon === "vector"}
-  <Vector />
-{:else}
-  MISSING ICON
-{/if}
+<svelte:component this={iconMap[icon]} />
