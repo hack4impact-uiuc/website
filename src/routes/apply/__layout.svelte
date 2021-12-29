91ce284d9cabc5c64b447058b1273588a1b0901c
Arpan Laha <arpanlaha99@gmail.com>
2021-12-29 19:31:53 +0000

    add redirect workaround

---
+++ src/routes/apply/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" context="module">
+  export async function load({ page }) {
+    return {
+      status: 301,
+      redirect: page.path.replace("apply", "join"),
+    };
+  }
+</script>
+
+<slot />
