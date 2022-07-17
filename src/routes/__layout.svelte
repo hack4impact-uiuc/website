2634936b16c767cee5f695daa27828b12cc82e45
AndrewLester <alester220@gmail.com>
2022-07-16 21:16:52 -0400

    Only run ackee if variables exist

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,6 @@
   import Nav from "$components/Nav.svelte";
   import Footer from "$components/Footer.svelte";
   import type { Load } from "@sveltejs/kit";
-  import "../app.css";
 
   export const load: Load = async ({ url, fetch }) => {
     const res = await fetch("/server/project-semesters.json");
@@ -16,15 +15,21 @@
 <script lang="ts">
   import * as ackeeTracker from "ackee-tracker";
   import { beforeUpdate } from "svelte";
+  import "../app.css";
 
   export let semesters: string[] = [];
   export let path: string;
 
-  beforeUpdate(() =>
-    ackeeTracker
-      .create(import.meta.env.CLIENT_ACKEE_URL)
-      .record(import.meta.env.CLIENT_ACKEE_DOMAIN_ID)
-  );
+  if (
+    import.meta.env.CLIENT_ACKEE_URL &&
+    import.meta.env.CLIENT_ACKEE_DOMAIN_ID
+  ) {
+    beforeUpdate(() =>
+      ackeeTracker
+        .create(import.meta.env.CLIENT_ACKEE_URL)
+        .record(import.meta.env.CLIENT_ACKEE_DOMAIN_ID)
+    );
+  }
 </script>
 
 <Nav {path} />
