c84cbf8927984a781f30f4a32bc850ab2d700b5c
AndrewLester <alester220@gmail.com>
2022-12-18 16:31:07 -0500

    Remove comment because it IS prerendered

--- src/routes/apply/[page=apply_redirect]/+page.ts
+++ src/routes/apply/[page=apply_redirect]/+page.ts
@@ -2,7 +2,6 @@ import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 // Redirect old links to apply/(nonprofits|sponors|students) to new page join/$1
-// This won't be prerendered because of the route parameter "page"
 export const load: PageLoad = async ({ url }) => {
   throw redirect(301, url.pathname.replace("apply", "join"));
 };
