f5f2c4af7382dfda969c9ce25ee7f0cc0d13c125
AndrewLester <alester220@gmail.com>
2022-12-18 15:39:23 -0500

    Fix check and apply some settings from new SvelteKit default

--- src/routes/apply/[page=apply_redirect]/+page.ts
+++ src/routes/apply/[page=apply_redirect]/+page.ts
@@ -1,8 +1,8 @@
 import { redirect } from "@sveltejs/kit";
-import type { LayoutLoad } from "../$types";
+import type { PageLoad } from "./$types";
 
 // Redirect old links to apply/(nonprofits|sponors|students) to new page join/$1
 // This won't be prerendered because of the route parameter "page"
-export const load: LayoutLoad = async ({ url }) => {
+export const load: PageLoad = async ({ url }) => {
   throw redirect(301, url.pathname.replace("apply", "join"));
 };
