b2f97441c6b62e4807deceed03d30d738ae483c3
AndrewLester <alester220@gmail.com>
2022-12-21 18:02:34 -0500

    Add skip link and fix missing types in load

---
+++ src/lib/components/SkipLink.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+</script>
+
+<div class="skip-link-wrapper">
+  <Button href="#content" type="primary">Skip to content</Button>
+</div>
+
+<style>
+  .skip-link-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 1px;
+    height: 1px;
+    margin: 0;
+    overflow: hidden;
+    clip: rect(1px, 1px, 1px, 1px);
+  }
+
+  .skip-link-wrapper:focus-within {
+    z-index: 9999;
+    width: auto;
+    height: auto;
+    clip: auto;
+  }
+</style>
--- src/routes/join/sponsors/+page.server.ts
+++ src/routes/join/sponsors/+page.server.ts
@@ -2,6 +2,7 @@ import {
   contentWrapper,
   getTestimonialNonprofit,
 } from "$lib/content/contentful";
+import type { FAQ } from "$lib/utils/schema";
 
 import type { PageServerLoad } from "./$types";
 
@@ -11,7 +12,7 @@ export const load: PageServerLoad = () => {
     faqs: contentWrapper.get("faq", {
       order: "fields.order",
       "fields.category": "Sponsor",
-    }),
+    }) as Promise<FAQ[]>,
     testimonialNonprofit: getTestimonialNonprofit(),
   };
 };
