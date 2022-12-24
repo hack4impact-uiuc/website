6b9509061746d25ff368b147f8f13aadf28c63cd
AndrewLester <alester220@gmail.com>
2022-12-23 21:53:41 -0500

    Fix type checking

--- src/lib/utils/animatedDetails.ts
+++ src/lib/utils/animatedDetails.ts
@@ -11,7 +11,9 @@ export default function animatedDetails(
     transitioning = true;
 
     const heightKeyframes = [
-      `${element.firstElementChild?.offsetHeight}px`,
+      `${
+        (element.firstElementChild as HTMLElement | undefined)?.offsetHeight
+      }px`,
       `${element.offsetHeight}px`,
     ];
 
