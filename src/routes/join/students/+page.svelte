9f63c2c3f75afcbad1f3ed577eeb461f730fbc14
Anya Parekh <49364484+anyaparekh@users.noreply.github.com>
2025-08-22 12:36:34 -0500

    Update Application Process Information (#94)
    * Update Application Process Information
    
    ---------
    
    Co-authored-by: Saket Reddy <144753536+SaketR3@users.noreply.github.com>

--- src/routes/join/students/+page.svelte
+++ src/routes/join/students/+page.svelte
@@ -39,16 +39,12 @@
 
 <Section color="var(--gray-lighter)" padding="40px">
   <h2 id="process">Application Process</h2>
-  <p>{@html data.info.applicationBlurb}</p>
-  <div id="process-steps">
-    {#each data.applicationSteps as step}
-      <Step icon={iconMap[step.icon]}>
-        <span slot="name">{step.name}</span>
-        <span slot="date">{step.date ?? ""}</span>
-        <span slot="description">{@html step.description}</span>
-      </Step>
-    {/each}
-  </div>
+  <p>
+    Submit your application by 11:59 PM CDT on Sunday, August 31st! The
+    application can be found here: <a href="https://h4i.app/ly"
+      >https://h4i.app/ly</a
+    >
+  </p>
 </Section>
 
 {#if data.faqs.length > 0}
